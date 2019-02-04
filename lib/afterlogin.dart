41b457f9f7f9a1dc149318cccda41f9333a00356
Fardeen Khan <khanfardeen9983@gmail.com>
2019-02-04 17:39:41 +0530

    HTTP call to get event list

--- lib/afterlogin.dart
+++ lib/afterlogin.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 //import 'user.dart' as user;
 
@@ -11,77 +13,73 @@ class AfterLogin extends StatefulWidget {
 
 class _AfterLogin extends State<AfterLogin> {
   final FirebaseAuth auth = FirebaseAuth.instance;
+  List<Widget> eventChildren = List();
   @override
   // TODO: implement widget
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-                accountName: Text("Fardeen Khan"), accountEmail: null),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_box),
-              title: Text("Log Out"),
-              onTap: () async {
-                await auth.signOut();
-                Navigator.of(context).pop();
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => AfterSplash()));
-              },
-            ),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text('AfterLogin'),
-      ),
-      body: Container(
-        child: Padding(
-          padding: EdgeInsets.all(18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        drawer: Drawer(
+          child: ListView(
             children: <Widget>[
-              GestureDetector(
-                onTap: () {},
-                child: Card(
-                  elevation: 8.0,
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(48.0),
-                      child: Text(
-                        'Event_1',
-                        style: TextStyle(fontSize: 32.0),
-                      ),
-                    ),
-                  ),
-                ),
+              UserAccountsDrawerHeader(
+                  accountName: Text("Fardeen Khan"), accountEmail: null),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
               ),
-              GestureDetector(
-                onTap: () {},
-                child: Card(
-                  elevation: 8.0,
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(48.0),
-                      child: Text(
-                        'Event_2',
-                        style: TextStyle(fontSize: 32.0),
-                      ),
-                    ),
-                  ),
-                ),
-              )
+              ListTile(
+                leading: Icon(Icons.account_box),
+                title: Text("Log Out"),
+                onTap: () async {
+                  await auth.signOut();
+                  Navigator.of(context).pop();
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => AfterSplash()));
+                },
+              ),
             ],
           ),
         ),
+        appBar: AppBar(
+          title: Text('AfterLogin'),
+        ),
+        body: Builder(builder: (BuildContext buildContext) {
+          fetch();
+          return Container(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: eventChildren),
+            ),
+          );
+        }));
+  }
+
+  void fetch() async {
+//    Response response =
+//        await get(Uri.encodeFull(""), headers: {"Accept": "application/json"});
+//    var json = jsonDecode(response.body);
+    var widgets = <Widget>[];
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
       ),
-    );
+    ));
+
+    setState(() {
+      eventChildren = widgets;
+    });
   }
 }
