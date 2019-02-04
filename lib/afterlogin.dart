ff210a0cf2d8548e4b2f47b6be10e5785a2f46d4
Fardeen Khan <khanfardeen9983@gmail.com>
2019-02-04 17:58:13 +0530

    Added scrollability to event list

--- lib/afterlogin.dart
+++ lib/afterlogin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login.dart';
+import 'user.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
@@ -14,6 +15,13 @@ class AfterLogin extends StatefulWidget {
 class _AfterLogin extends State<AfterLogin> {
   final FirebaseAuth auth = FirebaseAuth.instance;
   List<Widget> eventChildren = List();
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
+
   @override
   // TODO: implement widget
   Widget build(BuildContext context) {
@@ -22,7 +30,7 @@ class _AfterLogin extends State<AfterLogin> {
           child: ListView(
             children: <Widget>[
               UserAccountsDrawerHeader(
-                  accountName: Text("Fardeen Khan"), accountEmail: null),
+                  accountName: Text(username), accountEmail: null),
               ListTile(
                 leading: Icon(Icons.home),
                 title: Text("Home"),
@@ -44,13 +52,10 @@ class _AfterLogin extends State<AfterLogin> {
           title: Text('AfterLogin'),
         ),
         body: Builder(builder: (BuildContext buildContext) {
-          fetch();
           return Container(
             child: Padding(
               padding: EdgeInsets.all(18.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: eventChildren),
+              child: ListView(children: eventChildren),
             ),
           );
         }));
@@ -61,6 +66,118 @@ class _AfterLogin extends State<AfterLogin> {
 //        await get(Uri.encodeFull(""), headers: {"Accept": "application/json"});
 //    var json = jsonDecode(response.body);
     var widgets = <Widget>[];
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
+    widgets.add(GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 8.0,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Text(
+              'Event_1',
+              style: TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+      ),
+    ));
     widgets.add(GestureDetector(
       onTap: () {},
       child: Card(
